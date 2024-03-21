18086b4b448c593ff5a4bedf6a6adbec10938951
Stathis Vlachos <vlachos.stathis@gmail.com>
2024-03-21 18:38:18 +0200

    code examples

---
+++ rea_c1_u4/example01.R
@@ -0,0 +1,17 @@
+# ?iris
+
+dat = iris[, 1:4]
+
+pmatrix = scale(dat)
+
+d = dist(pmatrix)
+
+c = hclust(d, method="ward.D2")
+
+plot(c)
+
+rect.hclust(c, k=3)
+
+groups <- cutree(c, k=3)
+
+table(iris$Species, groups)
---
+++ rea_c1_u4/example02.R
@@ -0,0 +1,16 @@
+totwinss =c()
+
+for (k in 2:10) {
+  k_cl = kmeans(pmatrix, k)
+  totwinss[k] <- k_cl$tot.withinss
+}
+
+plot(1:10, totwinss,
+     xlab = "Number of clusters",
+     ylab = "Totel within ss")
+
+lines(1:10, totwinss)
+
+k_cl = kmeans(pmatrix, 3)
+
+table(iris$Species, k_cl$cluster)
